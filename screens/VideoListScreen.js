9a0caf3f9bb8d0ce7569969680bc8bfbee5d5baa
Jonathan Engelsma <jengelsma@gmail.com>
2021-06-05 13:00:48 -0400

    Part G

--- screens/VideoListScreen.js
+++ screens/VideoListScreen.js
@@ -1,6 +1,8 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, TouchableOpacity, View } from "react-native";
+import { Image, ListItem } from 'react-native-elements';
 import React, {useEffect, useState} from "react";
 
+import { Avatar } from "react-native-elements/dist/avatar/Avatar";
 import { BaseRouter } from "@react-navigation/native";
 import { getVideos } from '../api/YTServer';
 
@@ -10,16 +12,29 @@ const VideoListScreen = ({ navigation }) => {
 
   useEffect(() => {
     getVideos((data) => {
-      // console.log('received: ', data);
+      console.log('received: ', data.items[0].snippet.title);
       setVideos(data.items);
     });
   }, []);
 
   const renderVideo = ({index, item}) => {
     return (
-      <View>
-        <Text> {item.snippet.title} </Text>
-      </View>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("Video Viewer", item);
+        }}
+      >
+        <ListItem key={index}>
+          <Image 
+            source={{uri: item.snippet.thumbnails.default.url}}
+            style={{width: 100, height: 55}}
+          />
+          <ListItem.Content>
+            <ListItem.Title> {item.snippet.title} </ListItem.Title>
+          </ListItem.Content>
+          <ListItem.Chevron />
+        </ListItem>
+      </TouchableOpacity>
     );
   };
 
